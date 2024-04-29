620e13c56456d4913504b007f58e790d14aeba59
Scott Yeargain <Scott.Yeargain@csat.com>
2024-04-29 15:27:09 -0500

    initial

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 using OidcProxy.Net.ModuleInitializers;
-using OidcProxy.Net.OpenIdConnect;
+using OidcProxy.Net.Okta;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var config = builder.Configuration
-    .GetSection("OidcProxy")
-    .Get<OidcProxyConfig>();
+var config = builder.Configuration.GetSection("OidcProxy").Get<OktaProxyConfig>();
+//builder.Services.AddOidcProxy(config);
 
 builder.Services.AddOidcProxy(config);
+//    (o =>
+//{
+
+//    o.RegisterIdentityProvider<OktaIdentityProvider, OktaConfig>(config.Okta);
+//    o.ConfigureYarp(o =>
+//    {
+//        o.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+//    });
+//});
+
+
 
 var app = builder.Build();
 
